57c5b2b465df4fb52d828e83ddb45283a2564163
robinson nemeth <robinsonflavio@gmail.com>
2022-09-11 18:12:39 -0400

    todoList project

---
+++ todoList/app/Http/Controllers/TodoListController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\ListItem;
+
+class TodoListController extends Controller
+{
+    public function index(){
+        return view('welcome', ['listItems' => ListItem::where('is_complete',0)->get()]);
+    }
+
+    public function saveItem(Request $request){
+
+        $newListItem = new ListItem;
+        $newListItem->name = $request->listItem;
+        $newListItem->is_complete = 0;
+        $newListItem->save();
+
+        return redirect('/');
+    }
+
+    public function markComplete($id){
+        $listItem = ListItem::find($id);
+        $listItem->is_complete = 1;
+        $listItem->save();
+        return redirect('/');
+    }
+}
